6baac2069547c5ab589a3fc74a8fa9fdbd6e91e8
charan432 <sricharantumula@gmail.com>
2020-01-09 19:19:39 +0530

    updated files

--- foodfun/settings.py
+++ foodfun/settings.py
@@ -161,6 +161,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Setting Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
