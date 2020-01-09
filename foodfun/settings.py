2c7c4db4cfc8d9c053937a4231f10d7fce6aa584
charan432 <sricharantumula@gmail.com>
2020-01-09 19:50:48 +0530

    updated files

--- foodfun/settings.py
+++ foodfun/settings.py
@@ -116,7 +116,9 @@ WSGI_APPLICATION = 'foodfun.wsgi.application'
     #}
 #}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=600) }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
